43f82a18fb47bc90f41d94c2d26b8a6a8930c083
Asteinhart <45241656+Asteinhart@users.noreply.github.com>
2025-07-17 18:22:18 -0400

    lib/

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,51 @@
+<footer class="footer p-5">
+	<div class="container">
+		<div class="columns">
+			<div class="column">
+				<h5 class="title is-5 mb-3 title-font">Open Peru</h5>
+				<div class="social-icons">
+					<a href="#" class=" is-size-3 mr-3">
+						<i class="fa fa-youtube"></i>
+					</a>
+					<a href="#" class=" is-size-3">
+						<i class="fa fa-twitter"></i>
+					</a>
+				</div>
+			</div>
+
+			<div class="column">
+				<h5 class="title is-6 mb-3 title-font">Status</h5>
+				<ul>
+					<li><a href="/products/web" class=" is-size-7">Congresistas</a></li>
+					<li><a href="/products/mobile" class=" is-size-7">Bills</a></li>
+				</ul>
+			</div>
+
+			<div class="column">
+				<h5 class="title is-6 mb-3 title-font">Data</h5>
+				<ul>
+					<li><a href="/docs" class=" is-size-7">Download the data</a></li>
+					<li><a href="/blog" class=" is-size-7">API documentation</a></li>
+				</ul>
+			</div>
+
+			<div class="column">
+				<h5 class="title is-6 mb-1 title-font">About</h5>
+				<ul>
+					<li><a href="/privacy" class=" is-size-7">Our Team</a></li>
+					<li><a href="/terms" class=" is-size-7">Blog</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<style>
+	* {
+		background-color: var(--black);
+		color: white;
+	}
+	footer a:hover {
+		color: var(--blue) !important;
+	}
+</style>
---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+	// You can add any JavaScript logic here
+	import op_logo from '../assets/op_logo.svg';
+</script>
+
+<header class="navbar is-light p-1" role="navigation" aria-label="main navigation">
+	<div class="navbar-brand is-flex is-align-items-center is-justify-content-centerå">
+		<img src={op_logo} alt="Logo" class="image mr-1 is-64x64" />
+		<span class="navbar-item title title-font is-4 pl-1">Open Peru</span>
+	</div>
+
+	<div class="navbar-menu">
+		<div class="navbar-end">
+			<a href="/congressistas" class="navbar-item title-font">Congresistas</a>
+			<a href="/noticias" class="navbar-item title-font">Bills</a>
+			<a href="/about" class="navbar-item title-font">About</a>
+			<a href="/espanol" class="navbar-item title-font">
+				<span class="button is-primary">Español</span>
+			</a>
+		</div>
+	</div>
+</header>
+
+<style>
+</style>
